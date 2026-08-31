d7527ed3f5a77b27fee5cae2bce6a0ed45ed0635
192524092simats-blip <192524092.simats@saveetha.com>
2026-08-31 11:13:14 +0530

    Add files via upload

---
+++ three adderss code.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    char a, b, c;
+
+    printf("Enter Expression (a+b): ");
+    scanf("%c%c%c", &a, &b, &c);
+
+    printf("\nThree Address Code:\n");
+
+    printf("t1 = %c %c %c\n", a, b, c);
+
+    printf("\nOptimized Output: t1");
+
+    return 0;
+}
+
+
